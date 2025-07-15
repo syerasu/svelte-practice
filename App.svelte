47dcc33171ec0f55aee182cbfb0f5deced1be9ce
syerasu <syerasu99@gmail.com>
2025-07-15 15:25:25 -0400

    testing

---
+++ App.svelte
@@ -0,0 +1,25 @@
+<script>
+import ImageExample from './src/sections/ImageExample.svelte';
+import LeftAlignedExample from "./src/sections/LeftAlignedExample.svelte";
+import RightAlignedExample from "./src/sections/RightAlignedExample.svelte";
+import TitleSection from "./src/sections/TitleSection.svelte";
+import InteresectionObserverBasicExample from "./src/sections/InteresectionObserverBasicExample.svelte";
+import IntersectionObserverAlertExample from "./src/sections/IntersectionObserverAlertExample.svelte";
+import DisappearingDuckExample from "./src/sections/DisappearingDuckExample.svelte";
+import IntersectionObserverSectionCard from "./src/sections/IntersectionObserverSectionCard.svelte";
+import IntersectionObserverListExample from "./src/sections/IntersectionObserverListExample.svelte";
+</script>
+
+<main>
+  <ImageExample />
+  <TitleSection />
+  <RightAlignedExample />
+  <LeftAlignedExample />
+  <ImageExample />
+
+  <IntersectionObserverSectionCard />
+  <InteresectionObserverBasicExample />
+  <IntersectionObserverAlertExample />
+  <DisappearingDuckExample />
+  <IntersectionObserverListExample />
+</main>
---
+++ TestSection.svelte
@@ -0,0 +1,24 @@
+<script>
+  import Scroller from '../lib/Scroller.svelte'
+  import ArticleText from '../lib/ArticleText.svelte'
+</script>
+
+<Scroller layout="left">
+  {#snippet sticky()}
+    <h2>This stays still</h2>
+  {/snippet}
+
+  {#snippet scrolly()}
+    <ArticleText>
+      <p>This scrolls into view.</p>
+    </ArticleText>
+
+    <ArticleText>
+      <p>Here’s more scrolling content.</p>
+    </ArticleText>
+
+    <ArticleText>
+      <p>Add as many blocks as you want!</p>
+    </ArticleText>
+  {/snippet}
+</Scroller>
